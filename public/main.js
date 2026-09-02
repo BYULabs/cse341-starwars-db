25778e00bd21a9307920c85c41da674384f3b674
Sebastian Iturralde <itusebastian@gmail.com>
2026-09-02 17:56:44 -0500

    Add event listeners for updating and deleting Darth Vader's quote

--- public/main.js
+++ public/main.js
@@ -1,6 +1,8 @@
-const update = document.querySelector('#update-button')
+const update = document.querySelector('#update-button');
+const deleteButton = document.querySelector('#delete-button');
+const messageDiv = document.querySelector('#message');
 
-update.addEventListener('click', _ => {
+update.addEventListener('click', () => {
   fetch('/quotes', {
     method: 'put',
     headers: { 'Content-Type': 'application/json' },
@@ -9,4 +11,36 @@ update.addEventListener('click', _ => {
       quote: 'I find your lack of faith disturbing.',
     }),
   })
-})
+    .then(res => {
+      if (res.ok) return res.json();
+    })
+    .then(response => {
+      if (response === 'No quote found to update') {
+        messageDiv.textContent = 'No Yoda quote to replace';
+      } else {
+        window.location.reload(); // Reloads the UI to render updated quotes
+      }
+    })
+    .catch(error => console.error(error));
+});
+
+deleteButton.addEventListener('click', () => {
+  fetch('/quotes', {
+    method: 'delete',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      name: 'Darth Vader'
+    })
+  })
+    .then(res => {
+      if (res.ok) return res.json();
+    })
+    .then(response => {
+      if (response === 'No Darth Vader quote to delete') {
+        messageDiv.textContent = 'No Darth Vader quote to delete';
+      } else {
+        window.location.reload();
+      }
+    })
+    .catch(error => console.error(error));
+});
